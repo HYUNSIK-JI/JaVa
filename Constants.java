592a4257e228200482fd9f6ccc8144fc10dcd212
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-07 22:37:05 +0900

    상수

---
+++ Constants.java
@@ -0,0 +1,12 @@
+class Constants{
+	public static void main(String[] args){
+		final int MAX_SIZE=100;
+		final char CONST_Char='상';
+		final int CONST_ASSIGEND;
+		CONST_ASSIGEND=12;
+
+		System.out.println("상수1:"+MAX_SIZE);
+		System.out.println("상수2:"+CONST_Char);
+		System.out.println("상수3:"+CONST_ASSIGEND);
+	}
+}
