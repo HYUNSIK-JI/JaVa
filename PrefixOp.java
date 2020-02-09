841570028c177098b77acacdb1a2f2e6d222ee34
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-09 14:37:15 +0900

    PrefixOp

---
+++ PrefixOp.java
@@ -0,0 +1,8 @@
+class PrefixOp{
+	public static void main(String[] args){
+		int num=7;
+		System.out.println(++num); //num의값 하나 증가후 출력.
+		System.out.println(++num); //num의값 하나 증가후 출력.
+		System.out.println(num);
+	}
+}
