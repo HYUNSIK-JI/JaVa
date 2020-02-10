ba70eceb771890137e8f59aea9a112fa1409426e
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-10 15:33:58 +0900

    문제 05-3-1.java

---
+++ 문제05-3-1.java
@@ -0,0 +1,17 @@
+class ¹®Á¦05_3_1{
+	public static void main(String[] args){
+		int n=3;
+		
+		if (n==1){
+			System.out.println("Simple Java");
+			}
+		if (n==2){
+			System.out.println("Funny Java");
+			}
+		else if(n==3){
+			System.out.println("Fantastic Java");
+			}
+		System.out.println("The best programming language");
+		System.out.println("Do you like Java?");
+	}
+}
