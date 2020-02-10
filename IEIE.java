6023109715c286390be3bba819d710b2d33347d4
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-10 14:33:57 +0900

    if~else문

---
+++ IEIE.java
@@ -0,0 +1,12 @@
+class IEIE{
+	public static void main(String[] args){
+		int num=100;
+
+		if(num<0)
+			System.out.println("0미만");
+		else if(num<100)
+			System.out.println("100미만");
+		else
+			System.out.println("100이상");
+	}
+}
