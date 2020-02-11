f8908faf493c2ffb92966f511fcd7a161bf19c8e
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-11 14:37:43 +0900

    문제05_4_2

---
+++ 문제05_4_2.java
@@ -0,0 +1,15 @@
+class ¹®Á¦05_4_2{
+	public static void main(String[] args){
+		int num=1;
+
+		while(num<100){
+		System.out.println(num);
+		num++;
+		}
+
+		do{
+		System.out.println(num);
+		num--;	
+		}while(num>=1);
+	}
+}
