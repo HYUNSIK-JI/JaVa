e04eaf0e894bf239b3b5fa3e24a2b79036b85dbc
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-11 14:34:47 +0900

    문제05_4_1

---
+++ 문제05_4_1.java
@@ -0,0 +1,12 @@
+class ¹®Á¦05_4_1{
+	public static void main(String[] args){
+		int num=1;
+		int total=0;
+
+		while(num<100){
+			total+=num;
+			num++;
+		}
+		System.out.println("ÃÑÇÕ:"+total);
+	}
+}
