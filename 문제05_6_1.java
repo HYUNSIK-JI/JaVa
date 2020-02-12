9c25385990b4511df8281036b20fa5b725579092
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-12 15:37:52 +0900

    문제05_6_1

---
+++ 문제05_6_1.java
@@ -0,0 +1,14 @@
+class ¹®Á¦05_6_1{
+	public static void main(String[] args){
+		int num=0;
+		int count=0;
+
+		for(num=1; num<100; num++){
+			if(((num%5)!=0) ||((num%7)!=0))
+				continue;
+			count++;
+			System.out.println(num);
+		}
+		System.out.println("Count:"+count);
+	}
+}
