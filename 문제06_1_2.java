fa57376a0acd01ea7622820d85894b65b70d9aee
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-14 23:49:53 +0900

    문제06_1_2

---
+++ 문제06_1_2.java
@@ -0,0 +1,16 @@
+class ¹®Á¦06_1_2{
+	public static void main(String[] args){
+	int a=3;
+	int b=16;
+	result(a,b);
+	}
+	public static void result(int a,int b){
+	int c=a-b;
+	if(c<0){
+		c=-c;
+		System.out.println(c);
+		}
+	else
+		System.out.println(c);
+	}
+}
