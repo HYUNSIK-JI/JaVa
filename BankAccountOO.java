2c73012f4e96ae4de56895917a5bb7e5306e8ec0
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-16 16:58:21 +0900

    BankAccount1

---
+++ BankAccountOO.java
@@ -0,0 +1,35 @@
+class BankAccountOO{
+	public static void main(String[] args){
+	//두개의 인스턴스 생성
+	BankAccount yoon=new BankAccount();
+	BankAccount park=new BankAccount();
+
+	//각 인스턴스를 대상으로 예금을 진행
+	yoon.deposit(5000);
+	park.deposit(3000);
+
+	//각 인스턴스를 대상으로 출금을 진행
+	yoon.withdraw(2000);
+	park.withdraw(2000);
+
+	//각 인스턴스를 대상으로 잔액을 조회
+	yoon.checkmyBalance();
+	park.checkmyBalance();
+	}
+}
+class BankAccount{
+	int balance=0;
+
+	public int deposit(int amount) {
+		balance+=amount;
+		return balance;
+	}
+	public int withdraw(int amount) {
+		balance-=amount;
+		return balance;
+	}
+	public  int checkmyBalance() {
+		System.out.println("잔액:"+balance);
+		return balance;
+	}
+}
