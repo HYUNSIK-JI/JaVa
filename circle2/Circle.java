14af9527b4541611b4c61e864612ddca6f37e1c7
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-18 15:00:12 +0900

    같은이름 다른 패키지

---
+++ circle2/Circle.java
@@ -0,0 +1,15 @@
+package com.wxfx.smart; //패키지 선언
+
+public class Circle{
+	double rad;
+	final double PI;
+	
+public Circle(double r){
+	rad=r;
+	PI=3.14;
+}
+
+public double getArea(){
+	return (rad*rad)*PI;
+	}
+}
