5610917929b1a64daa7a14e974dfe81de5e81f9f
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-18 14:14:05 +0900

    현재디렉토리를기준으로 한 실행

---
+++ whatyourname.java
@@ -0,0 +1,19 @@
+class whatyourname{
+	public static void main(String[] args){
+		AAA aaa=new AAA();
+		aaa.showname();
+		
+		ZZZ zzz=new ZZZ();
+		zzz.showname();
+	}
+}
+class AAA{
+	public void showname(){
+		System.out.println("MY name is AAA");
+	}
+}
+class ZZZ{
+	public void showname(){
+		System.out.println("MY name is ZZZ");
+	}
+}
