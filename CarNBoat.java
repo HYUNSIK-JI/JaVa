f97da0c6960bd4045eab8f045272b79b5b944f9d
HYUNSIK-JI <wlgustlraaa@naver.com>
2020-02-23 04:28:30 +0900

    main메소드의 위치

---
+++ CarNBoat.java
@@ -0,0 +1,19 @@
+class CarNBoat{
+	public static void main(String[] args){
+	Car c=new Car();
+	c.mycar();
+
+	Boat t=new Boat();
+	t.myBoat();
+	}
+}
+class Car{
+	void mycar(){
+	System.out.println("This is mycar");
+	}
+}
+class Boat{
+	void myBoat(){
+	System.out.println("This is myboat");
+	}
+}
